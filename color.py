d69e85f4c097d02b17f41eccef1ae9bb1a546b37
prmts23 <35394919+prmts23@users.noreply.github.com>
2022-11-13 21:24:12 +0300

    Create color.py

---
+++ color.py
@@ -0,0 +1,29 @@
+
+def sketch_image(photo, k_size, scale ):
+    #Read Image
+    img=cv2.imread(photo)
+    
+    # Convert to Grey Image
+    grey_img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Invert Image
+    invert_img=cv2.bitwise_not(grey_img)
+    #invert_img=255-grey_img
+
+    # Blur image
+    blur_img=cv2.GaussianBlur(invert_img, (k_size,k_size),0)
+
+    # Invert Blurred Image
+    invblur_img=cv2.bitwise_not(blur_img)
+    #invblur_img=255-blur_img
+
+    # Sketch Image
+    sketch_img=cv2.divide(grey_img,invblur_img, scale=scale)
+
+    # Save Sketch 
+    cv2.imwrite('sketch.jpg', sketch_img)
+
+    # Display sketch
+    #cv2_imshow(sketch_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
